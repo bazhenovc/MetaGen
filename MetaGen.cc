b6c93b166957f8df4a89e80e0d83021f0e20f56b
bazhenovc <bazhenovc@gmail.com>
2013-07-12 10:35:27 +0300

    first commit

---
+++ MetaGen.cc
@@ -0,0 +1,292 @@
+#if 0
+#pragma comment (lib, "LLVMAArch64AsmParser.lib")
+#pragma comment (lib, "LLVMAArch64AsmPrinter.lib")
+#pragma comment (lib, "LLVMAArch64CodeGen.lib")
+#pragma comment (lib, "LLVMAArch64Desc.lib")
+#pragma comment (lib, "LLVMAArch64Disassembler.lib")
+#pragma comment (lib, "LLVMAArch64Info.lib")
+#pragma comment (lib, "LLVMAArch64Utils.lib")
+#pragma comment (lib, "LLVMAnalysis.lib")
+#pragma comment (lib, "LLVMArchive.lib")
+#pragma comment (lib, "LLVMARMAsmParser.lib")
+#pragma comment (lib, "LLVMARMAsmPrinter.lib")
+#pragma comment (lib, "LLVMARMCodeGen.lib")
+#pragma comment (lib, "LLVMARMDesc.lib")
+#pragma comment (lib, "LLVMARMDisassembler.lib")
+#pragma comment (lib, "LLVMARMInfo.lib")
+#pragma comment (lib, "LLVMAsmParser.lib")
+#pragma comment (lib, "LLVMAsmPrinter.lib")
+#pragma comment (lib, "LLVMBitReader.lib")
+#pragma comment (lib, "LLVMBitWriter.lib")
+#pragma comment (lib, "LLVMCodeGen.lib")
+#pragma comment (lib, "LLVMCore.lib")
+#pragma comment (lib, "LLVMCppBackendCodeGen.lib")
+#pragma comment (lib, "LLVMCppBackendInfo.lib")
+#pragma comment (lib, "LLVMDebugInfo.lib")
+#pragma comment (lib, "LLVMExecutionEngine.lib")
+#pragma comment (lib, "LLVMHexagonAsmPrinter.lib")
+#pragma comment (lib, "LLVMHexagonCodeGen.lib")
+#pragma comment (lib, "LLVMHexagonDesc.lib")
+#pragma comment (lib, "LLVMHexagonInfo.lib")
+#pragma comment (lib, "LLVMInstCombine.lib")
+#pragma comment (lib, "LLVMInstrumentation.lib")
+#pragma comment (lib, "LLVMInterpreter.lib")
+#pragma comment (lib, "LLVMipa.lib")
+#pragma comment (lib, "LLVMipo.lib")
+#pragma comment (lib, "LLVMIRReader.lib")
+#pragma comment (lib, "LLVMJIT.lib")
+#pragma comment (lib, "LLVMLinker.lib")
+#pragma comment (lib, "LLVMMBlazeAsmParser.lib")
+#pragma comment (lib, "LLVMMBlazeAsmPrinter.lib")
+#pragma comment (lib, "LLVMMBlazeCodeGen.lib")
+#pragma comment (lib, "LLVMMBlazeDesc.lib")
+#pragma comment (lib, "LLVMMBlazeDisassembler.lib")
+#pragma comment (lib, "LLVMMBlazeInfo.lib")
+#pragma comment (lib, "LLVMMC.lib")
+#pragma comment (lib, "LLVMMCDisassembler.lib")
+#pragma comment (lib, "LLVMMCJIT.lib")
+#pragma comment (lib, "LLVMMCParser.lib")
+#pragma comment (lib, "LLVMMipsAsmParser.lib")
+#pragma comment (lib, "LLVMMipsAsmPrinter.lib")
+#pragma comment (lib, "LLVMMipsCodeGen.lib")
+#pragma comment (lib, "LLVMMipsDesc.lib")
+#pragma comment (lib, "LLVMMipsDisassembler.lib")
+#pragma comment (lib, "LLVMMipsInfo.lib")
+#pragma comment (lib, "LLVMMSP430AsmPrinter.lib")
+#pragma comment (lib, "LLVMMSP430CodeGen.lib")
+#pragma comment (lib, "LLVMMSP430Desc.lib")
+#pragma comment (lib, "LLVMMSP430Info.lib")
+#pragma comment (lib, "LLVMNVPTXAsmPrinter.lib")
+#pragma comment (lib, "LLVMNVPTXCodeGen.lib")
+#pragma comment (lib, "LLVMNVPTXDesc.lib")
+#pragma comment (lib, "LLVMNVPTXInfo.lib")
+#pragma comment (lib, "LLVMObjCARCOpts.lib")
+#pragma comment (lib, "LLVMObject.lib")
+#pragma comment (lib, "LLVMOption.lib")
+#pragma comment (lib, "LLVMPowerPCAsmParser.lib")
+#pragma comment (lib, "LLVMPowerPCAsmPrinter.lib")
+#pragma comment (lib, "LLVMPowerPCCodeGen.lib")
+#pragma comment (lib, "LLVMPowerPCDesc.lib")
+#pragma comment (lib, "LLVMPowerPCInfo.lib")
+#pragma comment (lib, "LLVMRuntimeDyld.lib")
+#pragma comment (lib, "LLVMScalarOpts.lib")
+#pragma comment (lib, "LLVMSelectionDAG.lib")
+#pragma comment (lib, "LLVMSparcCodeGen.lib")
+#pragma comment (lib, "LLVMSparcDesc.lib")
+#pragma comment (lib, "LLVMSparcInfo.lib")
+#pragma comment (lib, "LLVMSupport.lib")
+#pragma comment (lib, "LLVMSystemZAsmParser.lib")
+#pragma comment (lib, "LLVMSystemZAsmPrinter.lib")
+#pragma comment (lib, "LLVMSystemZCodeGen.lib")
+#pragma comment (lib, "LLVMSystemZDesc.lib")
+#pragma comment (lib, "LLVMSystemZInfo.lib")
+#pragma comment (lib, "LLVMTableGen.lib")
+#pragma comment (lib, "LLVMTarget.lib")
+#pragma comment (lib, "LLVMTransformUtils.lib")
+#pragma comment (lib, "LLVMVectorize.lib")
+#pragma comment (lib, "LLVMX86AsmParser.lib")
+#pragma comment (lib, "LLVMX86AsmPrinter.lib")
+#pragma comment (lib, "LLVMX86CodeGen.lib")
+#pragma comment (lib, "LLVMX86Desc.lib")
+#pragma comment (lib, "LLVMX86Disassembler.lib")
+#pragma comment (lib, "LLVMX86Info.lib")
+#pragma comment (lib, "LLVMX86Utils.lib")
+#pragma comment (lib, "LLVMXCoreAsmPrinter.lib")
+#pragma comment (lib, "LLVMXCoreCodeGen.lib")
+#pragma comment (lib, "LLVMXCoreDesc.lib")
+#pragma comment (lib, "LLVMXCoreDisassembler.lib")
+#pragma comment (lib, "LLVMXCoreInfo.lib")
+#pragma comment (lib, "profile_rt.lib")
+
+#pragma comment(lib, "clangAnalysis.lib")
+#pragma comment(lib, "clangARCMigrate.lib")
+#pragma comment(lib, "clangAST.lib")
+#pragma comment(lib, "clangASTMatchers.lib")
+#pragma comment(lib, "clangBasic.lib")
+#pragma comment(lib, "clangCodeGen.lib")
+#pragma comment(lib, "clangDriver.lib")
+#pragma comment(lib, "clangEdit.lib")
+#pragma comment(lib, "clangFormat.lib")
+#pragma comment(lib, "clangFrontend.lib")
+#pragma comment(lib, "clangFrontendTool.lib")
+#pragma comment(lib, "clangLex.lib")
+#pragma comment(lib, "clangParse.lib")
+#pragma comment(lib, "clangRewriteCore.lib")
+#pragma comment(lib, "clangRewriteFrontend.lib")
+#pragma comment(lib, "clangSema.lib")
+#pragma comment(lib, "clangSerialization.lib")
+#pragma comment(lib, "clangStaticAnalyzerCheckers.lib")
+#pragma comment(lib, "clangStaticAnalyzerCore.lib")
+#pragma comment(lib, "clangStaticAnalyzerFrontend.lib")
+#pragma comment(lib, "clangTooling.lib")
+#endif
+
+#ifdef _WIN32
+#pragma warning(push)
+#pragma warning(disable:4244)
+#pragma warning(disable:4624)
+#pragma warning(disable:4800)
+#pragma warning(disable:4146)
+#pragma warning(disable:4291)
+#pragma warning(disable:4355)
+#pragma warning(disable:4345)
+#endif
+
+#include <stdint.h>
+
+#include <llvm/Support/raw_ostream.h>
+#include <llvm/Support/Host.h>
+#include <llvm/ADT/IntrusiveRefCntPtr.h>
+
+#include <clang/Basic/LangOptions.h>
+#include <clang/Basic/FileManager.h>
+#include <clang/Basic/TargetOptions.h>
+#include <clang/Basic/TargetInfo.h>
+
+#include <clang/Lex/Preprocessor.h>
+#include <clang/Lex/HeaderSearch.h>
+
+#include <clang/Frontend/TextDiagnosticPrinter.h>
+#include <clang/Frontend/CompilerInstance.h>
+
+#include <clang/Sema/Sema.h>
+
+#include <clang/AST/ASTConsumer.h>
+#include <clang/AST/ASTContext.h>
+
+#include <clang/Parse/ParseAST.h>
+#include <clang/Parse/Parser.h>
+
+#ifdef _WIN32
+#pragma warning(pop)
+#endif
+
+#include <iostream>
+
+class ASTConsumer : public clang::ASTConsumer
+{
+public:
+
+	virtual void HandleTagDeclDefinition(clang::TagDecl* D) override
+	{
+		std::cout << "<class name=\"" << D->getNameAsString() << "\">" << std::endl;
+		clang::CXXRecordDecl* cxxrec = llvm::dyn_cast<clang::CXXRecordDecl>(D);
+		if (cxxrec) {
+
+			for (auto itr = cxxrec->bases_begin(); itr != cxxrec->bases_end(); ++itr) {
+				std::cout << "\t" << "<inherits name=\"" << itr->getType().getAsString() << "\" />" << std::endl;
+			}
+
+			for (auto itr = cxxrec->vbases_begin(); itr != cxxrec->vbases_end(); ++itr) {
+				std::cout << "\t" << "<virtual_inherits name=\"" << itr->getType().getAsString() << "\" />" << std::endl;
+			}
+
+			for (auto itr = cxxrec->method_begin(); itr != cxxrec->method_end(); ++itr) {
+				std::cout << "\t" << "<method name=\"" << itr->getNameAsString() << "\" return=\"" << itr->getResultType().getAsString() << "\" >" << std::endl;
+				for (auto parm_itr = itr->param_begin(); parm_itr != itr->param_end(); ++parm_itr) {
+					clang::VarDecl* vd = *parm_itr;
+
+					std::cout << "\t\t" << "<param name=\""  << vd->getNameAsString() << "\" type=\"" << vd->getType().getAsString() << "\">" << std::endl;
+				}
+				std::cout << "\t" << "</method>" << std::endl;
+			}
+
+			for (auto itr = cxxrec->field_begin(); itr != cxxrec->field_end(); ++itr) {
+				std::cout << "\t" << "<field name=\"" << itr->getNameAsString() << "\" type=\"" << itr->getType().getAsString() << "\" />" << std::endl;
+			}
+		}
+
+		std::cout << "</class>" << std::endl;
+	}
+};
+
+int main(int argc, char** argv)
+{
+	if (argc < 2) {
+		std::cerr << "Usage: MetaGen <filename>" << std::endl;
+		return 1;
+	}
+	clang::DiagnosticOptions diagnosticOptions;
+	clang::TextDiagnosticPrinter *pTextDiagnosticPrinter = new clang::TextDiagnosticPrinter(
+		llvm::outs(),
+		&diagnosticOptions,
+		true);
+	llvm::IntrusiveRefCntPtr<clang::DiagnosticIDs> pDiagIDs;
+
+	clang::DiagnosticsEngine *pDiagnosticsEngine = new clang::DiagnosticsEngine(pDiagIDs,
+									 &diagnosticOptions,
+									 pTextDiagnosticPrinter);
+
+	clang::LangOptions languageOptions;
+	languageOptions.CPlusPlus = 1;
+	languageOptions.CPlusPlus11 = 1;
+
+	clang::FileSystemOptions fileSystemOptions;
+
+	clang::FileManager fileManager(fileSystemOptions);
+
+	clang::SourceManager sourceManager(
+		*pDiagnosticsEngine,
+		fileManager);
+
+	clang::TargetOptions targetOptions;
+	targetOptions.Triple = llvm::sys::getDefaultTargetTriple();
+
+	clang::TargetInfo *pTargetInfo = clang::TargetInfo::CreateTargetInfo(
+			*pDiagnosticsEngine,
+			&targetOptions);
+
+	llvm::IntrusiveRefCntPtr<clang::HeaderSearchOptions> hso;
+	clang::HeaderSearch headerSearch(hso,
+									 fileManager,
+									 *pDiagnosticsEngine,
+									 languageOptions,
+									 pTargetInfo);
+
+	clang::CompilerInstance compInst;
+
+	llvm::IntrusiveRefCntPtr<clang::PreprocessorOptions> pOpts;
+
+	clang::Preprocessor preprocessor(
+		pOpts,
+		*pDiagnosticsEngine,
+		languageOptions,
+		pTargetInfo,
+		sourceManager,
+		headerSearch,
+		compInst);
+
+	const clang::FileEntry *pFile = fileManager.getFile(argv[1]);
+	if (pFile) {
+		sourceManager.createMainFileID(pFile);
+
+		const clang::TargetInfo &targetInfo = *pTargetInfo;
+
+		clang::IdentifierTable identifierTable(languageOptions);
+		clang::SelectorTable selectorTable;
+
+		clang::Builtin::Context builtinContext;
+		builtinContext.InitializeTarget(targetInfo);
+		clang::ASTContext astContext(
+			languageOptions,
+			sourceManager,
+			pTargetInfo,
+			identifierTable,
+			selectorTable,
+			builtinContext,
+			0 /* size_reserve*/);
+		ASTConsumer astConsumer;
+
+		clang::Sema sema(
+			preprocessor,
+			astContext,
+			astConsumer);
+
+		pTextDiagnosticPrinter->BeginSourceFile(languageOptions, &preprocessor);
+		clang::ParseAST(preprocessor, &astConsumer, astContext);
+		pTextDiagnosticPrinter->EndSourceFile();
+		identifierTable.PrintStats();
+	} else std::cerr << "File not found" << std::endl;
+
+	return 0;
+}
